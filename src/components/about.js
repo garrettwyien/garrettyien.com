29622cdaafa7b19612d52ff4fedb6e00685703b9
Garrett Yien <yien.garrett94@gmail.com>
2022-04-18 10:18:50 -0400

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -16,7 +16,7 @@ export default function About () {
     return (
         <StyledAbout>
             <h3>About me:</h3>
-            <p>I am an aspiring programmer located in New York. I graduated from Tulane University with a degree in psychology and pursued a career in social work for several years after. Wanting to broaden my horizons and expand the ways I could add value to any work that I do, I attended and completed a full stack web development course at the <a href="https://www.bloomtech.com/">Bloom Institute of Technology (formerly Lambda School)</a>. Currently, I am excited to take on whatever challenges that may lie ahead and continue to develop my skills and experience.</p>
+            <p>I am an aspiring programmer located in New York. I graduated from Tulane University with a degree in psychology and pursued a career in social work for several years after. Wanting to broaden my horizons and expand the ways I could add value to any work that I do, I attended and completed a full stack web development course at the <a href="https://www.bloomtech.com/">Bloom Institute of Technology (formerly Lambda School)</a>. Currently, I am working as an IT Consultant at FDM Group.</p>
         </StyledAbout>
     );
 };
